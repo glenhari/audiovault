b322eebaf44767b4e664b0127dbcd96096fec631
Guilherme Lenhari <glenhari>
2023-07-30 20:04:54 +0000

    Initial commit

---
+++ src/main/kotlin/io/com/audiovault/Application.kt
@@ -0,0 +1,7 @@
+package io.com.audiovault
+
+import io.micronaut.runtime.Micronaut.run
+fun main(args: Array<String>) {
+	run(*args)
+}
+
---
+++ src/test/kotlin/io/com/audiovault/AudiovaultTest.kt
@@ -0,0 +1,12 @@
+package io.com.audiovault
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class AudiovaultTest(private val application: EmbeddedApplication<*>): StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
